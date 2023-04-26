9cb820374922694b58cbc93b7b874218cc179f2a
RashadHamidli <mr_rashad@email.com>
2023-04-26 10:43:23 +0400

    add

---
+++ ResumeDesktopApp/src/main/java/com/company/config/Config.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.config;
+
+import com.company.entity.User;
+
+/**
+ *
+ * @author mr_rashad
+ */
+public class Config {
+
+    public static User loggedInUser;
+
+}
