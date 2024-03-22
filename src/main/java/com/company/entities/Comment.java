9d47d8104822b1976e27f1546b84dd8b54b4276e
RashadHamidli <mr_rashad@email.com>
2024-03-22 19:07:50 +0400

    add

--- src/main/java/com/company/entities/Comment.java
+++ src/main/java/com/company/entities/Comment.java
@@ -5,16 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
-public class Post {
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "post text must not be empty")
+    @NotBlank(message = "comment text must not be empty")
     @Column(columnDefinition = "TEXT")
     private String text;
     @NotNull
