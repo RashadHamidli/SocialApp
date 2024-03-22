4c8aeb9883ed0542d70387a863b7747f000d0a6e
RashadHamidli <mr_rashad@email.com>
2024-03-22 19:04:14 +0400

    add

---
+++ src/main/java/com/company/entities/Post.java
@@ -0,0 +1,23 @@
+package com.company.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "text must not be empty")
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    @NotNull
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime createDate;
+}
--- src/main/java/com/company/entities/User.java
+++ src/main/java/com/company/entities/User.java
@@ -12,25 +12,26 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "username must be not null")
+    @NotBlank(message = "username must not be empty")
     @Size(min = 2, max = 20, message = "required username must be min 2, max 20 character")
     @Column(unique = true)
     private String username;
-    @NotBlank(message = "name must be not null")
+    @NotBlank(message = "name must not be empty")
     @Size(min = 2, max = 20, message = "required name must be min 2, max 20 character")
     private String name;
-    @NotBlank(message = "surname must be not null")
+    @NotBlank(message = "surname must not be empty")
     @Size(min = 2, max = 20, message = "required surname must be min 2, max 20 character")
     private String surname;
-    @NotBlank(message = "email must be not null")
+    @NotBlank(message = "email must not be empty")
     @Column(unique = true)
     @Email
     private String email;
-    @NotBlank(message = "password must be not null")
+    @NotBlank(message = "password must not be empty")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$",
             message = "Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 digit, with a minimum length of 8 characters")
     private String password;
-    @NotNull(message = "birthdate must be not null")
+    @NotNull(message = "birthdate must not be empty")
+    @Column(columnDefinition = "DATE")
     private LocalDate birthdate;
 
 
