b97895d4cf8d070cc091029a9873c3c489f606ad
RashadHamidli <mr_rashad@email.com>
2024-03-23 12:51:00 +0400

    add

--- src/main/java/com/company/dto/response/CommentResponse.java
+++ src/main/java/com/company/dto/response/CommentResponse.java
@@ -4,11 +4,16 @@ import com.company.entities.Comment;
 
 import java.time.LocalDateTime;
 
-public record CommentResponse(String username,
-                              Long id,
+public record CommentResponse(String postUsername,
+                              String commentUsername,
+                              Long commentId,
                               String text,
                               LocalDateTime createDate) {
     public static CommentResponse converteCommentToCommentResponse(Comment comment) {
-        return new CommentResponse(comment.getUser().getUsername(), comment.getPost().getPost_id(), comment.getText(), comment.getCreateDate());
+        return new CommentResponse(comment.getPost().getUser().getUsername(),
+                comment.getUser().getUsername(),
+                comment.getPost().getPost_id(),
+                comment.getText(),
+                comment.getCreateDate());
     }
 }
