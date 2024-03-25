49b0b470d807d9c0559baf2f50c11a943afef092
RashadHamidli <email_rashad@mail.ru>
2024-03-25 11:26:19 +0400

    add

---
+++ src/main/java/com/company/entities/Token.java
@@ -0,0 +1,22 @@
+package com.company.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "tokens")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tokenId;
+    @NotBlank
+    private String token;
+    @NotNull
+    private LocalDateTime createToke;
+}
