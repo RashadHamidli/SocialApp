5cb55ca418ff1ac767cf6ec407a37c82c080c472
RashadHamidli <online.carats@gmail.com>
2024-03-25 11:28:02 +0400

    add

--- src/main/java/com/company/entities/Token.java
+++ src/main/java/com/company/entities/Token.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
